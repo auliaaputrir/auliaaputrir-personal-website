8bed5c237d98fd34b9bc6390a5357013e13c2aac
auliaaputrir <auliaputrirachmadani@gmail.com>
2025-02-21 21:37:23 +0700

    social media

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,6 +1,9 @@
 import LinkMenu from "../LinkMenu"
 import { TfiAlignJustify } from "react-icons/tfi";
 import { useState } from "react";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
 
 export default function Header() {
     const [isOpen, setIsOpen] = useState(false)
@@ -26,6 +29,11 @@ export default function Header() {
                             <li>
                                 <LinkMenu to='/portofolio'>Portofolio</LinkMenu>
                             </li>
+                            <ul className="flex gap-4 text-lg text-center mt-1">
+                                <li><a href="https://github.com/auliaaputrir" className="hover:text-sky-600"><FaGithub /></a></li>
+                                <li><a href="https://www.linkedin.com/in/auliaaputrir/" className="hover:text-sky-600"><FaLinkedinIn /></a></li>
+                                <li><a href="mailto:auliaputrirachmadani@gmail.com>" className="hover:text-sky-600"><HiOutlineMail /></a></li>
+                            </ul>
                         </ul>
                     </div>
                     {isOpen && (
@@ -40,6 +48,11 @@ export default function Header() {
                                 <li>
                                     <LinkMenu to='/portofolio'>Portofolio</LinkMenu>
                                 </li>
+                                <ul className="flex gap-4 text-lg mt-1 justify-center">
+                                    <li><a href="https://github.com/auliaaputrir" className="hover:text-sky-600"><FaGithub /></a></li>
+                                    <li><a href="https://www.linkedin.com/in/auliaaputrir/" className="hover:text-sky-600"><FaLinkedinIn /></a></li>
+                                    <li><a href="mailto:auliaputrirachmadani@gmail.com>" className="hover:text-sky-600"><HiOutlineMail /></a></li>
+                                </ul>
                             </ul>
 
                         </div>
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,15 +1,14 @@
 import { Outlet } from "react-router"
 import Header from '../components/header'
-import Aside from "../components/aside"
+
 
 export default function MainLayout() {
     return (
         <>
             <div className="min-h-screen w-full subpixel-antialiased bg-white">
                 <Header />
-                <main className="lg:pb-5 lg:pb-20 py-10 lg:mx-24 mx-10 flex items-center">
+                <main className="relative lg:pb-5 lg:pb-20 py-10 lg:mx-24 mx-10 flex items-center">
                     <Outlet />
-                    <Aside />
                 </main>
             </div>
         </>
